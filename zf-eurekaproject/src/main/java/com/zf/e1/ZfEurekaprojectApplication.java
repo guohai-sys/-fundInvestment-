e4b7a4110ea5097f7b897106d4d688b4ab595682
11548 <11548@DESKTOP-TRRE2AI>
2020-04-15 20:47:54 +0800

    xxx

---
+++ zf-eurekaproject/src/main/java/com/zf/e1/ZfEurekaprojectApplication.java
@@ -0,0 +1,15 @@
+package com.zf.e1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class ZfEurekaprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZfEurekaprojectApplication.class, args);
+	}
+
+}
---
+++ zf-eurekaproject/src/test/java/com/zf/e1/ZfEurekaprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zf.e1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZfEurekaprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
